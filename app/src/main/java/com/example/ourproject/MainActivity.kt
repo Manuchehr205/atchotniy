32a342613ed99d886782f7499d8e3a3ded6c7ea2
Manuchehr <manucehr205gmail.com>
2023-03-31 16:38:35 +0500

    Version 1.0

--- app/src/main/java/com/example/ourproject/MainActivity.kt
+++ app/src/main/java/com/example/ourproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.ourproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var textView = findViewById<TextView>(R.id.view)
+        var button = findViewById<Button>(R.id.button)
+
     }
 }
